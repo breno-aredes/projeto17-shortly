3f5baec10c8d60b2e873ae57a792e7780bf34c2e
breno-aredes <breno_mg10@hotmail.com>
2023-02-14 11:27:11 -0300

    feat: server connected to postgreSQL

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-e;
+import express from "express";
+import cors from "cors";
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+//server.use([adicionar as rotas aqui])
+
+server.listen(process.env.PORT, console.log("servidor conectado na PORT"));
---
+++ src/database/database.connection.js
@@ -0,0 +1,14 @@
+import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { Pool } = pkg;
+
+const configDataBase = {
+  connectionString: process.env.DATABASE_URL,
+};
+
+if (process.env.MODE === "prod") configDataBase.ssl = true;
+
+export const db = new Pool(configDataBase);
